332a00580022bb1643af442476e191ce017cb1ff
tilak-atpl <tilak.ram@autopeepal.com>
2024-07-31 09:37:22 +0530

    this is commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2>Practicing the deployment process.</h2>
+
+      <p>Happy! After Deploy This Simple Project!!!</p>
+    </div>
+  );
+}
+
+export default App;
