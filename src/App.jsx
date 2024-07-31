d3380cddb0db91727729013b3e05e13875699cbf
tilak-atpl <tilak.ram@autopeepal.com>
2024-07-31 10:50:48 +0530

    deployed 5th time,  and integrated the ngix on the server don't need to restart even the server😆

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <p>Wooooo....Babe deployed second time😆😆😆😝😝😝🤣</p>
       <p>Wooooo....Babe deployed third time😆 and prepared build folder locally😆😆😝😝😝🤣</p>
       <p>Now,Integrated the nginx on the server</p>
+      <p>Wooooo....Babe deployed 5th time😆,prepared build folder locally😆 and integrated the ngix on the server don't need to restart even the server😆😝😝😝🤣</p>
     </div>
   );
 }
